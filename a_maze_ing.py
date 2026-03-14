002a384b2dba891a4b777ec2328ccd7ebd08c1b8
sP3ed^oN. <ahraka2002@gmail.com>
2026-03-14 15:10:19 +0000

    algo done

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -4,28 +4,28 @@ import sys
 def parse_config(file_config: str) -> dict:
     config = {}
     try:
-        with open(file_config, "r") as file:
+        with open(file_config, 'r') as file:
             for line in file:
-                if not line.strip() or line.strip().startswith("#"):
+                if not line.strip() or line.strip().startswith('#'):
                     continue
                 part = line.split("=")
                 key = part[0].strip()
                 value = part[1].strip()
                 config[key] = value
         for key in config:
-            if key == "WIDTH":
+            if key == 'WIDTH':
                 config[key] = int(config[key])
-            elif key == "HEIGHT":
+            elif key == 'HEIGHT':
                 config[key] = int(config[key])
-            elif key == "ENTRY":
-                parts = config[key].split(",")
+            elif key == 'ENTRY':
+                parts = config[key].split(',')
                 config[key] = (int(parts[0]), int(parts[1]))
-            elif key == "EXIT":
-                parts = config[key].split(",")
+            elif key == 'EXIT':
+                parts = config[key].split(',')
                 config[key] = (int(parts[0]), int(parts[1]))
-            elif key == "PERFECT":
+            elif key == 'PERFECT':
                 config[key] = config[key].lower() in ("true", "1", "yes")
-            elif key == "SEED":
+            elif key == 'SEED':
                 config[key] = int(config[key])
         return config
     except FileNotFoundError as e:
