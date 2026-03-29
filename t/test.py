2d7b7742fe933d34b8c2ba754d9ff6ee888c71c9
Ferdaous El Idrissi <feel-idr@e2r12p9.1337.ma>
2026-03-29 17:44:01 +0100

    add test.py to test package

--- t/test.py
+++ t/test.py
@@ -1,13 +1,14 @@
 from mazegen.maze import Maze
 from mazegen.generator import Generator
 
-def main():
-	maze = Maze(10, 10, (0, 0), (0, 5), "maze.txt")
-	gen = Generator(10, 10, (0, 0), (0, 5), None, True, "maze.txt")
-	gen.generate()
-	maze = gen.get_maze()
-    
-	print(maze.ascii_render())
+
+def main() -> None:
+    maze = Maze(10, 10, (0, 0), (0, 5), "maze.txt")
+    gen = Generator(10, 10, (0, 0), (0, 5), None, True, "maze.txt")
+    gen.generate()
+    maze = gen.get_maze()
+
+    print(maze.ascii_render())
+
 
 main()
-
